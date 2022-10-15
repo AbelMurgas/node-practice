a528f9d421c1f7d02be4499906b007c3a4ac9a95
AbelMurgas <abelmurgasdev@gmail.com>
2022-10-15 00:25:29 -0500

    add method to obtain data

--- PurchaseManagement/PurchaseManagement.js
+++ PurchaseManagement/PurchaseManagement.js
@@ -1,13 +1,11 @@
 const csv = require('csv-parser');
 const fs = require('fs');
-<<<<<<< HEAD
 const rf = require('./ReadFileCSV')
 
 const read = new rf.ReadFileCSV("hola","hi", "nomames", "ok");
 console.log(read.obtain_table_object())
 
 
-=======
 
 fs.createReadStream('product.csv')
   .pipe(csv())
@@ -17,4 +15,3 @@ fs.createReadStream('product.csv')
   .on('end', () => {
     console.log('CSV file successfully processed');
   });
->>>>>>> dee8b96f2431925f7398598d5e60ec175f568b9d
