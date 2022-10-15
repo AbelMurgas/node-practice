95a9826dd9830db991ef8abba5a95dbeca662b48
AbelMurgas <abelmurgasdev@gmail.com>
2022-10-15 16:33:42 -0500

    finish project

--- PurchaseManagement/PurchaseManagement.js
+++ PurchaseManagement/PurchaseManagement.js
@@ -1,17 +1,57 @@
-const csv = require('csv-parser');
-const fs = require('fs');
-const rf = require('./ReadFileCSV')
+class PurchaseManagement {
+  #product;
+  #purchase;
+  #listProductsTotalSales;
 
-const read = new rf.ReadFileCSV("hola","hi", "nomames", "ok");
-console.log(read.obtain_table_object())
+  constructor(tables) {
+    this.#product = tables.product;
+    this.#purchase = tables.purchase;
+    this.#listProductsTotalSales = this.#getListProductsTotalSales(tables);
+  }
 
+  #getListProductsTotalSales = ({ purchase, product }) => {
+    return product.map((element) => {
+      return {
+        id: element.id,
+        description: element.description,
+        totalSale: purchase.reduce((accumulator, object) => {
+          return object.product === element.id
+            ? accumulator + object.quantity * element.price
+            : accumulator;
+        }, 0),
+      };
+    });
+  };
 
+  getOrderListProductsTotalSalesDesc() {
+    return this.#orderProductsByTotalSalesDesc(this.#listProductsTotalSales);
+  }
 
-fs.createReadStream('product.csv')
-  .pipe(csv())
-  .on('data', (row) => {
-    console.log(row);
-  })
-  .on('end', () => {
-    console.log('CSV file successfully processed');
-  });
+  #orderProductsByTotalSalesDesc = (listProductsTotalSales) => {
+    return listProductsTotalSales.sort((producto1, producto2) => {
+      return producto1.totalSale === producto2.totalSale
+        ? 0
+        : producto1.totalSale > producto2.totalSale
+        ? -1
+        : 1;
+    });
+  };
+
+  getListSoldProduct = () => {
+    return this.#product.filter((obj) => {
+      return this.#listProductsTotalSales.some((element) => {
+        return obj.id === element.id && element.totalSale > 0;
+      });
+    });
+  };
+
+  getListUnSoldProduct = () => {
+    return this.#product.filter((obj) => {
+      return this.#listProductsTotalSales.some((element) => {
+        return obj.id === element.id && element.totalSale === 0;
+      });
+    });
+  };
+}
+
+module.exports = PurchaseManagement;
