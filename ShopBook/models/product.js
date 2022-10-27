14abcd554cfb01b7e6334ccbb3cb1972fa96336d
AbelMurgas <abelmurgasdev@gmail.com>
2022-10-27 08:33:49 -0500

    add data models

--- ShopBook/models/product.js
+++ ShopBook/models/product.js
@@ -1,16 +1,41 @@
-const products = [];
+const fs = require("fs");
 
-module.exports = class Product{
+const path = require("path");
 
-    constructor(title){
-        this.title = title;
-    }
+const pathModule = require("../utils/path");
+//const products = [];
 
-    save(){
-        products.push(this)
-    }
+module.exports = class Product {
+  constructor(title) {
+    this.title = title;
+  }
 
-    static fetchAll() {
-        return products;
-    }
-}
+  save() {
+    const p = path.join(pathModule, "data", "products.json");
+    fs.readFile(p, (err, data) => {
+      let products = [];
+      if (!err) {
+        products = JSON.parse(data);
+      } else {
+        console.log(err);
+      }
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), (err) => {
+        if (err) {
+          console.log(err);
+        }
+      });
+    });
+  }
+
+  static fetchAll(callback) {
+    const p = path.join(pathModule, "data", "products.json");
+    fs.readFile(p, (err, data) => {
+      if (err) {
+        console.log(err);
+        callback([]);
+      }
+      callback(JSON.parse(data));
+    });
+  }
+};
