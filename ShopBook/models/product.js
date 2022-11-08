648c14b6c4ccdaf7cd03695f6a3434fb0113dd0d
AbelMurgas <abelmurgasdev@gmail.com>
2022-11-08 01:03:04 -0500

    add other features to th product module

--- ShopBook/models/product.js
+++ ShopBook/models/product.js
@@ -5,12 +5,32 @@ const path = require("path");
 const pathModule = require("../utils/path");
 //const products = [];
 
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  'data',
+  'products.json'
+);
+
+const getProductsFromFile = cb => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
+    }
+  });
+};
+
 module.exports = class Product {
-  constructor(title) {
+  constructor(title, imageUrl, price, description) {
     this.title = title;
+    this.imageUrl = imageUrl;
+    this.price = price;
+    this.description = description
   }
 
   save() {
+    this.id = Math.random().toString();
     const p = path.join(pathModule, "data", "products.json");
     fs.readFile(p, (err, data) => {
       let products = [];
@@ -29,13 +49,42 @@ module.exports = class Product {
   }
 
   static fetchAll(callback) {
-    const p = path.join(pathModule, "data", "products.json");
-    fs.readFile(p, (err, data) => {
-      if (err) {
-        console.log(err);
-        callback([]);
-      }
-      callback(JSON.parse(data));
-    });
+    getProductsFromFile(callback)
+  }
+
+  static fetch(idProduct ,callback) {
+    getProductsFromFile(products => {
+      const product = products.find(p => p.id === idProduct);
+      callback(product)
+    })
+  }
+
+  static updateByID(productObject, callback){
+    getProductsFromFile(produts => {
+      const newProducts = produts.map(prod => {
+        if (prod.id === productObject.id){
+          return productObject
+        }
+        return prod
+      })
+      fs.writeFile(p, JSON.stringify(newProducts), (err) => {
+        if (err) {
+          console.log(err);
+        }
+        callback()
+      });
+    })
+  }
+
+  static deleteByID(productId, callback){
+    getProductsFromFile(produts => {
+      const newProducts = produts.filter(prod => prod.id !== productId)
+      fs.writeFile(p, JSON.stringify(newProducts), (err) => {
+        if (err) {
+          console.log(err);
+        }
+        callback()
+      });
+    })
   }
 };
