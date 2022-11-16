b6faf29bcf4f7671ef56c4c1fa111f0a5d24dcd6
AbelMurgas <abelmurgasdev@gmail.com>
2022-11-16 11:51:10 -0500

    change product to sequelize

--- ShopBook/models/product.js
+++ ShopBook/models/product.js
@@ -1,35 +1,27 @@
-const db = require("../utils/database");
+const Sequelize = require('sequelize')
 
-module.exports = class Product {
-  constructor(title, imageUrl, price, description) {
-    this.title = title;
-    this.imageUrl = imageUrl;
-    this.price = price;
-    this.description = description;
-  }
+const sequelize = require('../utils/database');
 
-  save() {
-    return db.execute(
-      "INSERT INTO products (title, price, imageURL, description) VALUES (?, ?, ?, ?)",
-      [this.title, this.price, this.imageUrl, this.description]
-    );
+const Product = sequelize.define('product', {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey:true
+  },
+  title: Sequelize.STRING,
+  price: {
+    type: Sequelize.DOUBLE,
+    allowNull: false
+  },
+  imageURL: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  description:{
+    type: Sequelize.STRING,
+    allowNull: false
   }
+});
 
-  static fetchAll() {
-    return db.execute("SELECT * FROM products");
-  }
-
-  static fetchByID(idProduct) {
-    return db.execute("SELECT * FROM products WHERE id = ?", [idProduct]);
-  }
-
-  static updateByID(productObject) {
-    return db.execute(
-      "UPDATE products SET title = ?, price = ?, description = ?, imageURL = ? WHERE id = ?", [productObject.title, productObject.price, productObject.description, productObject.imageUrl, productObject.id]
-    );
-  }
-
-  static deleteByID(productId) {
-    return db.execute("DELETE FROM products WHERE id = ?", [productId]);
-  }
-};
+module.exports = Product;
