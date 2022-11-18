7fa4beb6dcee06f4c0eeadd0379ce6d7e46a3f48
AbelMurgas <abelmurgasdev@gmail.com>
2022-11-18 12:31:53 -0500

    add features to app

--- ShopBook/app.js
+++ ShopBook/app.js
@@ -3,6 +3,11 @@ const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 
+const sequelize = require("./utils/database");
+const Product = require("./models/product");
+const User = require("./models/user");
+
+
 const app = express();
 
 app.set("view engine", "ejs");
@@ -12,14 +17,49 @@ const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 const errorController = require("./controllers/error");
 
-
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
+
+app.use((req, res, next) => {
+  User.findByPk(1)
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch();
+});
 
 app.use("/admin", adminRoutes.routes);
 app.use(shopRoutes.routes);
 
 app.use(errorController.get404);
 
-app.listen(3000);
+Product.belongsTo(User, { constraints: true, onDelete: "CASCADE" });
+User.hasMany(Product);
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product, { through: CartItem});
+Product.belongsToMany(Cart, { through: CartItem});
+
+sequelize
+  //.sync()
+  .sync({ force: true})
+  .then((result) => {
+    return User.findByPk(1);
+  })
+  .then((user) => {
+    if (!user) {
+      return User.create({
+        name: "Max",
+        email: "abelmurgasdev@gamil.com",
+        passWord: "1234",
+      });
+    }
+    return user;
+  })
+  .then((user) => {
+    app.listen(3000);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
