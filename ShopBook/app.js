6722aa64ae61dfa114837446b3f4e735b236a32f
AbelMurgas <abelmurgasdev@gmail.com>
2022-11-18 14:26:48 -0500

    updated controllers and app

--- ShopBook/app.js
+++ ShopBook/app.js
@@ -6,7 +6,8 @@ const bodyParser = require("body-parser");
 const sequelize = require("./utils/database");
 const Product = require("./models/product");
 const User = require("./models/user");
-
+const Cart = require("./models/cart");
+const CartItem = require("./models/cart-item");
 
 const app = express();
 
@@ -42,8 +43,8 @@ Cart.belongsToMany(Product, { through: CartItem});
 Product.belongsToMany(Cart, { through: CartItem});
 
 sequelize
-  //.sync()
-  .sync({ force: true})
+  .sync()
+  //.sync({ force: true})
   .then((result) => {
     return User.findByPk(1);
   })
@@ -56,6 +57,15 @@ sequelize
       });
     }
     return user;
+  })
+  .then(user =>{
+    return user.getCart();
+  })
+  .then(cart => {
+    if (cart){
+      return cart
+    }
+    return user.createCart();
   })
   .then((user) => {
     app.listen(3000);
--- ShopBook/routes/shop.js
+++ ShopBook/routes/shop.js
@@ -1,13 +1,18 @@
-const express = require('express');
+const express = require("express");
 
-const productController = require('../controllers/shop')
+const productController = require("../controllers/shop");
 
 const router = express.Router();
 
-router.get('/', productController.getIndex);
+router.get("/", productController.getIndex);
 
-router.get('/products', productController.getProducts)
+router.get("/products", productController.getProducts);
 
-router.get('/products/:productID', productController.getProduct)
+router.get("/products/:productID", productController.getProduct);
 
+router.get("/cart", productController.getCart);
+
+router.post("/cart", productController.postCart);
+
+router.post("/cart-delete-item", productController.postCartDeleteProduct);
 exports.routes = router;
