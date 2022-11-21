0eb013f1924ec344590f7546ba99c4740f619897
AbelMurgas <abelmurgasdev@gmail.com>
2022-11-21 17:02:36 -0500

    change DB Sql to noSql with mongoDb

--- ShopBook/app.js
+++ ShopBook/app.js
@@ -3,6 +3,10 @@ const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 
+const mongoConnect = require('./utils/database').mongoConnect;
+
+const User = require('./models/user')
+/*
 const sequelize = require("./utils/database");
 const Product = require("./models/product");
 const User = require("./models/user");
@@ -10,33 +14,46 @@ const Cart = require("./models/cart");
 const CartItem = require("./models/cart-item");
 const Order = require("./models/order");
 const OrderItem = require("./models/order-item");
+*/
 
 const app = express();
 
 app.set("view engine", "ejs");
 app.set("views", "views");
 
-const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
+const adminRoutes = require("./routes/admin");
 const errorController = require("./controllers/error");
+/*
+*/
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use((req, res, next) => {
+  User.findByPk('637bc218d1d4c9d215112d0d')
+    .then((user) => {
+      req.user = new User(user.name, user.email, user.cart , user._id)
+      next();
+    })
+    .catch();
+  /* --- SEQUELIZE
   User.findByPk(1)
     .then((user) => {
       req.user = user;
       next();
     })
     .catch();
+  */
 });
 
 app.use("/admin", adminRoutes.routes);
 app.use(shopRoutes.routes);
-
 app.use(errorController.get404);
 
+
+
+/* --- SEQUELIZE ---
 Product.belongsTo(User, { constraints: true, onDelete: "CASCADE" });
 User.hasMany(Product);
 User.hasOne(Cart);
@@ -74,10 +91,14 @@ sequelize
     }
     return user.createCart();
   })
-  */
+  
   .then(() => {
     app.listen(3000);
   })
   .catch((err) => {
     console.log(err);
   });
+*/
+mongoConnect((client) => {
+  app.listen(3000)
+})
