94228f92dc160d9f76a96842b146898df7c2ed69
AbelMurgas <abelmurgasdev@gmail.com>
2022-11-21 17:29:24 -0500

    update database to mongoDb information

--- ShopBook/utils/database.js
+++ ShopBook/utils/database.js
@@ -1,3 +1,38 @@
+const mongodb = require("mongodb");
+
+const config =  require('../config.js');
+
+const MongoClient = mongodb.MongoClient;
+
+let _db;
+
+const mongoConnect = (callback) => {
+  MongoClient.connect(
+    `mongodb+srv://${config.DBUSER}:${config.DBPASS}@cluster0.ssc1tcl.mongodb.net/?retryWrites=true&w=majority`
+  )
+    .then((client) => {
+      console.log("connected");
+      _db = client.db(config.DATABASE);
+      callback();
+    })
+    .catch((err) => {
+      console.log(err);
+      throw err;
+    });
+};
+
+const getDb = () => {
+  if (_db){
+    return _db;
+  }
+  throw 'No database found'
+}
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
+// module.exports = mongoConnect;
+
+/* --- SEQUELIZE ---
 const Sequelize = require("sequelize");
 
 const sequelize = new Sequelize("tutorials", "root", "212319972006", {
@@ -6,3 +41,4 @@ const sequelize = new Sequelize("tutorials", "root", "212319972006", {
 });
 
 module.exports = sequelize;
+*/
