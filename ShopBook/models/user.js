1b96af41588d78b9b6f136c5bdb25b527ca49b6e
AbelMurgas <abelmurgasdev@gmail.com>
2022-11-25 17:52:11 -0500

    finished cart, and orders

--- ShopBook/models/user.js
+++ ShopBook/models/user.js
@@ -58,24 +58,25 @@ class User {
       });
   }
 
-  async getCart() {
+  getCart() {
     const db = getDb();
     const productIds = this.cart.items.map((i) => {
       return i.productId;
     });
-    return db.collection('products')
-    .find({_id: { $in: productIds}})
-    .toArray()
-    .then(products => {
-     return products.map(p => {
-      return {
-       ...p,
-       quantity: this.cart.items.find((i) => {
-        return i.productId.toString() === p._id.toString();
-       }).quantity
-      }
-     })
-    })
+    return db
+      .collection("products")
+      .find({ _id: { $in: productIds } })
+      .toArray()
+      .then((products) => {
+        return products.map((p) => {
+          return {
+            ...p,
+            quantity: this.cart.items.find((i) => {
+              return i.productId.toString() === p._id.toString();
+            }).quantity,
+          };
+        });
+      });
 
     /* --- way to getCart before know find method with $in
     const products = await Promise.all(
@@ -92,6 +93,78 @@ class User {
       };
     });
     */
+  }
+
+  deleteItemFromCart(productId) {
+    const db = getDb();
+    const updCart = this.cart.items.filter((item) => {
+      return item.productId.toString() !== productId.toString(); // act cart items
+    });
+    console.log(this.cart.items);
+    return db
+      .collection("users")
+      .updateOne(
+        { _id: ObjectId(this._id) },
+        {
+          $set: {
+            cart: {
+              items: updCart,
+            },
+          },
+        }
+      )
+      .then((result) => {
+        console.log("User cart item Updated");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  addOrder() {
+    const db = getDb();
+    return this.getCart()
+      .then((products) => {
+        const order = {
+          items: products,
+          user: {
+            _id: this._id,
+            name: this.name,
+            emailUsed: this.email,
+          },
+        };
+        return order;
+      })
+      .then((order) => {
+        return db
+          .collection("orders")
+          .insertOne(order)
+          .then((result) => {
+            return db
+              .collection("users")
+              .updateOne(
+                { _id: ObjectId(this._id) },
+                {
+                  $set: {
+                    cart: {
+                      items: [],
+                    },
+                  },
+                }
+              )
+              .then((result) => {
+                console.log("User cart item Updated");
+              })
+              .catch((err) => {
+                console.log(err);
+              });
+          });
+      });
+  }
+
+  getOrders() {
+    const db = getDb();
+    return db.collection("orders").find({'user._id': this._id}).toArray();
   }
 
   static findByPk(idUser) {
