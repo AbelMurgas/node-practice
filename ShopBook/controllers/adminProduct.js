4e071f8144c9f79c4b1162b239e70b47113fdaa1
AbelMurgas <abelmurgasdev@gmail.com>
2022-12-07 16:34:16 -0500

    admin controller fix bugs and organize code

--- ShopBook/controllers/adminProduct.js
+++ ShopBook/controllers/adminProduct.js
@@ -1,5 +1,4 @@
 const product = require("../models/product");
-const Product = require("../models/product");
 
 exports.getAddProduct = (req, res, next) => {
   res.render("admin/add-product", {
@@ -7,30 +6,6 @@ exports.getAddProduct = (req, res, next) => {
     path: "/admin/add-product",
     isAuthenticated: req.session.isLoggedIn
   });
-};
-
-exports.postAddProduct = (req, res, next) => {
-  const title = req.body.title;
-  const imageUrl = req.body.imageUrl;
-  const price = req.body.price;
-  const description = req.body.description;
-  const product = new Product({
-    title: title,
-    price: price,
-    description: description,
-    imageUrl: imageUrl,
-    userId: req.session.user,
-  });
-  product
-    .save()
-    .then((result) => {
-      console.log("Create Product");
-      res.redirect("/admin/products");
-    })
-    .catch((err) => {
-      console.log(err);
-      res.redirect("/admin/add-product");
-    });
 };
 
 exports.getHomeAdmin = (req, res) => {
@@ -67,6 +42,30 @@ exports.getEditAdmin = (req, res) => {
     .catch((err) => console.log(err));
 };
 
+exports.postAddProduct = (req, res, next) => {
+  const title = req.body.title;
+  const imageUrl = req.body.imageUrl;
+  const price = req.body.price;
+  const description = req.body.description;
+  const product = new Product({
+    title: title,
+    price: price,
+    description: description,
+    imageUrl: imageUrl,
+    userId: req.session.user,
+  });
+  product
+    .save()
+    .then((result) => {
+      console.log("Create Product");
+      res.redirect("/admin/products");
+    })
+    .catch((err) => {
+      console.log(err);
+      res.redirect("/admin/add-product");
+    });
+};
+
 exports.postEditAdmin = (req, res) => {
   const id = req.body.id;
   const title = req.body.title;
@@ -97,72 +96,3 @@ exports.postDeleteAdmin = (req, res) => {
     })
     .catch((err) => console.log(err));
 };
-/* --- SEQUELIZE ----
-
-exports.getHomeAdmin = (req, res) => {
-  req.user.getProducts()
-    .then((products) => {
-      res.render("admin/products", {
-        prods: products,
-        pageTitle: "Products",
-        path: "/admin/products",
-        hasProducts: products.length > 0,
-        activeShop: true,
-        productCSS: true,
-      });
-    })
-    .catch((err) => console.log(err));
-};
-
-
-exports.getEditAdmin = (req, res) => {
-  const productId = req.params.productID;
-  req.user
-    .getProducts({ where: { id: productId } })
-    //Product.findByPk(productId)
-    .then((product) => {
-      product = product[0]
-      res.render("admin/edit-product", {
-        prod: product,
-        pageTitle: "Edit Product",
-        path: "",
-      });
-    })
-    .catch((err) => console.log(err));
-};
-
-exports.postEditAdmin = (req, res) => {
-  const id = req.body.id;
-  const title = req.body.title;
-  const imageUrl = req.body.imageUrl;
-  const price = req.body.price;
-  const description = req.body.description;
-  Product.findByPk(id)
-    .then((product) => {
-      product.title = title;
-      product.price = price;
-      product.imageURL = imageUrl;
-      product.description = description;
-      return product.save();
-    })
-    .then((result) => {
-      console.log("UPDATED PRODUCT");
-      res.redirect("/admin/products");
-    })
-    .catch((err) => console.log(err));
-};
-
-exports.postDeleteAdmin = (req, res) => {
-  const productId = req.body.id;
-  Product.findByPk(productId)
-    .then((product) => {
-      return product.destroy();
-    })
-    .then((result) => {
-      console.log("DELETED PRODUCT");
-      res.redirect("/admin/products");
-    })
-    .catch((err) => console.log(err));
-};
-
-*/
