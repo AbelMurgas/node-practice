eaadf4b0783760a94ae08fc38ad4c10305b2c6cd
AbelMurgas <abelmurgasdev@gmail.com>
2022-12-07 16:31:36 -0500

    add more global variable

--- ShopBook/config.js
+++ ShopBook/config.js
@@ -2,7 +2,7 @@ const dotenv = require('dotenv');
 const path = require('path');
 
 dotenv.config({
-    path: path.resolve(__dirname, `${process.env.NODE_ENV}.env`)
+    path: path.resolve(__dirname, `${process.env.NODE_ENV}`.trim() + '.env')
 });
 
 module.exports = {
@@ -11,5 +11,7 @@ module.exports = {
     PORT : process.env.PORT || 3000,
     DATABASE : process.env.DATABASE,
     DBUSER : process.env.DBUSER,
-    DBPASS : process.env.DBPASS
+    DBPASS : process.env.DBPASS,
+    EMAIL_API_KEY : process.env.EMAIL_API_KEY,
+    FROM_EMAIL: process.env.FROM_EMAIL
 }
