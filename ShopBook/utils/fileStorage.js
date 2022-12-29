1523ec9c4b49ac65a748072d4e1964e6fc1d87c5
AbelMurgas <abelmurgasdev@gmail.com>
2022-12-29 04:00:42 -0500

    create file storage with multer

---
+++ ShopBook/utils/fileStorage.js
@@ -0,0 +1,31 @@
+const multer = require("multer");
+
+const fileStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(
+      null,
+      new Date().toISOString().replace(/:/g, "-") + "-" + file.originalname
+    );
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  // reject a file
+  if (
+    file.mimetype === "image/jpeg" ||
+    file.mimetype === "image/jpg" ||
+    file.mimetype === "image/png"
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
+exports.dowloadImage = multer({
+  storage: fileStorage,
+  fileFilter: fileFilter,
+}).single("image");
